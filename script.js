6d7cf4099e444251b1898550efa13f19931589a5
amegha3103 <amegha3103@gmail.com>
2024-12-08 18:35:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+function addtocart(){
+    document.getElementById("Cart").innerHTML ="Added";
+    document.getElementById("Cart").style.backgroundColor="green";
+    document.getElementById("Cart").style.color="white";
+}
+
+function addtocart1(){
+  document.getElementById("Cart1").innerHTML ="Added";
+  document.getElementById("Cart1").style.backgroundColor="green";
+  document.getElementById("Cart1").style.color="white";
+}
+
+function addtocart2(){
+  document.getElementById("Cart2").innerHTML ="Added";
+  document.getElementById("Cart2").style.backgroundColor="green";
+  document.getElementById("Cart2").style.color="white";
+}
+
+function addtocart3(){
+  document.getElementById("Cart3").innerHTML ="Added";
+  document.getElementById("Cart3").style.backgroundColor="green";
+  document.getElementById("Cart3").style.color="white";
+}
+
+function addtocart4(){
+  document.getElementById("Cart4").innerHTML ="Added";
+  document.getElementById("Cart4").style.backgroundColor="green";
+  document.getElementById("Cart4").style.color="white";
+}
+
+function addtocart5(){
+  document.getElementById("Cart5").innerHTML ="Added";
+  document.getElementById("Cart5").style.backgroundColor="green";
+  document.getElementById("Cart5").style.color="white";
+}
+
+function addtocart6(){
+  document.getElementById("Cart6").innerHTML ="Added";
+  document.getElementById("Cart6").style.backgroundColor="green";
+  document.getElementById("Cart6").style.color="white";
+}
+
+function addtocart7(){
+  document.getElementById("Cart7").innerHTML ="Added";
+  document.getElementById("Cart7").style.backgroundColor="green";
+  document.getElementById("Cart7").style.color="white";
+}
+
+function addtocart8(){
+  document.getElementById("Cart8").innerHTML ="Added";
+  document.getElementById("Cart8").style.backgroundColor="green";
+  document.getElementById("Cart8").style.color="white";
+}
+
+function addtocart9(){
+  document.getElementById("Cart9").innerHTML ="Added";
+  document.getElementById("Cart9").style.backgroundColor="green";
+  document.getElementById("Cart9").style.color="white";
+}
+
+function addtocart10(){
+  document.getElementById("Cart10").innerHTML ="Added";
+  document.getElementById("Cart10").style.backgroundColor="green";
+  document.getElementById("Cart10").style.color="white";
+}
+
+function addtocart11(){
+  document.getElementById("Cart11").innerHTML ="Added";
+  document.getElementById("Cart11").style.backgroundColor="green";
+  document.getElementById("Cart11").style.color="white";
+}
+
+function addtocart12(){
+  document.getElementById("Cart12").innerHTML ="Added";
+  document.getElementById("Cart12").style.backgroundColor="green";
+  document.getElementById("Cart12").style.color="white";
+}
